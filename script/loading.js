ab01efead4f261a3b7f53c9846c2d0ef2d954d4a
Liana Lanier <favepink12@gmail.com>
2026-02-24 18:22:48 +0000

    Tue, Feb 24, 2026, 10:22 AM -08:00

--- script/loading.js
+++ script/loading.js
@@ -10,4 +10,8 @@ function blurring() {
     loadText.innerText = `${load}%`
     loadText.style.opacity = 1-load/100
     bg.style.filter = `blur(${50-load/2}px)`
+
+    if (load > 99) {
+        clearInterval(int)
+    }
 }
