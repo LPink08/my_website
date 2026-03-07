7fd949cdc7b69f7857b8b2ebca04a98455f6f1d5
Liana Lanier <favepink12@gmail.com>
2026-03-07 00:13:51 +0000

    Fri, Mar 6, 2026, 4:13 PM -08:00

--- script/expanding.js
+++ script/expanding.js
@@ -2,20 +2,27 @@ panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
-for (let i = 0; i < 5; i = i + 1) {
-    panels[i].addEventListener('click', () => {
-        removeActiveFromAllPanels()
-        panels[i].classList.add('active')
+function removeActiveClasses () {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
     })
 }
 
+// for (let i = 0; i < 5; i = i + 1) {
+//     panels[i].addEventListener('click', () => {
+//         removeActiveFromAllPanels()
+//         panels[i].classList.add('active')
+//     })
+// }
 
-function removeActiveFromAllPanels() {
-    for (let i = 0l i < 5; i = i + 1){
-        panels[i].classList.remove('active')
-    }
-}
+
+// function removeActiveFromAllPanels() {
+//     for (let i = 0l i < 5; i = i + 1){
+//         panels[i].classList.remove('active')
+//     }
+// }
