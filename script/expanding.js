54d7d666162ae90858c4b9c830f11a22ef089419
Liana Lanier <favepink12@gmail.com>
2026-03-06 16:27:01 -0800

    dk

--- script/expanding.js
+++ script/expanding.js
@@ -13,16 +13,3 @@ function removeActiveClasses () {
     })
 }
 
-// for (let i = 0; i < 5; i = i + 1) {
-//     panels[i].addEventListener('click', () => {
-//         removeActiveFromAllPanels()
-//         panels[i].classList.add('active')
-//     })
-// }
-
-
-// function removeActiveFromAllPanels() {
-//     for (let i = 0l i < 5; i = i + 1){
-//         panels[i].classList.remove('active')
-//     }
-// }
