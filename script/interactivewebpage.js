9b57710045e1695b6f3a4ed9165bf57252d779ad
Liana Lanier <favepink12@gmail.com>
2026-03-10 10:04:06 -0700

    dkfdjk

--- script/interactivewebpage.js
+++ script/interactivewebpage.js
@@ -4,7 +4,7 @@ colors = ['blue', 'green', 'purple', 'red', 'pink', 'orange', 'yellow']
 let index = 0
 
 box.addEventListener('click', () => {
-    clickBox.style.background = colors[index]
+    box.style.background = colors[index]
     index = index + 1
     if (index == colors.length){
         index = 0
