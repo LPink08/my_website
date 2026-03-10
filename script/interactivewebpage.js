5a734c4d4865ce6955e72a051389ac48c6af6bd7
Liana Lanier <favepink12@gmail.com>
2026-03-10 20:03:46 +0000

    Tue, Mar 10, 2026, 1:03 PM -07:00

--- script/interactivewebpage.js
+++ script/interactivewebpage.js
@@ -1,10 +1,11 @@
 box = document.querySelector('.box')
+body = document.querySelector('.body')
 
 colors = ['blue', 'green', 'purple', 'red', 'pink', 'orange', 'yellow']
 let index = 0
 
 box.addEventListener('click', () => {
-    style.body.background = colors[index]
+    body.style.background = colors[index]
     index = index + 1
     if (index == colors.length){
         index = 0
