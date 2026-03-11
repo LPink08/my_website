add142d5d508bdb6db89c49ec0facc706860d90c
Liana Lanier <favepink12@gmail.com>
2026-03-11 16:41:54 +0000

    Wed, Mar 11, 2026, 9:41 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -6,13 +6,18 @@ tada = document.querySelector('.tada')
 victory = document.querySelector('.victory')
 wrong = document.querySelector('.wrong')
 
-btn = document.createElement('button')
-btn.innerText = "applause"
-btn.classList.add('btn')
-btn.addEventListener('click', () => {
-    applause.play()
-})
-buttons.appendChild(btn)
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+
+for (let i = 0; i < 6; i = i + 1) {
+    btn = document.createElement('button')
+    btn.innerText = sounds[i]
+    btn.classList.add('btn')
+    btn.addEventListener('click', () => {
+        applause.play()
+    })
+    buttons.appendChild(btn)
+}
 
 
 
