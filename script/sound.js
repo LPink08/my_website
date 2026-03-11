51c8d6bb40eeec93c4f5490ed1a48cfa14e0931d
Liana Lanier <favepink12@gmail.com>
2026-03-11 16:45:15 +0000

    Wed, Mar 11, 2026, 9:45 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,13 +1,12 @@
 buttons = document.querySelector('.buttons')
-applause = document.querySelector('.applause')
-boo = document.querySelector('.boo')
-gasp = document.querySelector('.gasp')
-tada = document.querySelector('.tada')
-victory = document.querySelector('.victory')
-wrong = document.querySelector('.wrong')
+// applause = document.querySelector('.applause')
+// boo = document.querySelector('.boo')
+// gasp = document.querySelector('.gasp')
+// tada = document.querySelector('.tada')
+// victory = document.querySelector('.victory')
+// wrong = document.querySelector('.wrong')
 
 sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
-
 
 for (let i = 0; i < 6; i = i + 1) {
     btn = document.createElement('button')
