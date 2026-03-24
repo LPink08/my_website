44ace1a1e407bde431651cad869ea71f75a891c7
Liana Lanier <favepink12@gmail.com>
2026-03-24 02:43:56 +0000

    Mon, Mar 23, 2026, 7:43 PM -07:00

---
+++ script/insect.js
@@ -0,0 +1,85 @@
+const screens = document.querySelectorAll('.screen')
+
+screens[0].classList.add('up')
+// screens = document.querySelectorAll('.screen')
+// choose_insect_btns = document.querySelectorAll()
+
+
+
+
+// score = 0
+// seconds = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function startGame() {
+//     setTimeout(createInsect, 1000)
+//     setInterval(increaseTime, 1000)
+// }
+
+// function createInsect() {
+//     const insect = document.createElement('div')
+// }
+
+
+
+
+
+
+
+
+
+
+// function getRandomLocation() {
+
+}
+
+
+
+
+
+
+// function catchInsect(insect) {
+//     increaseScore()
+//     insect.classList.add('caught')
+//     set timeout ( () => insect.reomve(), 2000)
+//     addInsects()
+// }
+
+// function addInsects() {
+//     set timeout(createInsect, 1000)
+//     set timeout(createInsect, 1500)
+// }
+
+// function increaseScore() {
+//     score = score + 1
+//     scoreE1.innerHTML = `Score: ${score}`
+// }
+
+// function increaseTime() {
+//     seconds = seconds + 1
+//     m = Math.floor(seconds/60)
+//     s = seconds % 60
+//     if (m < 10){
+//         m = `0${m}`
+//     }
+//     if (s< 10){
+//         s = `0${s}`
+//     }
+
+//     timeE1.inerHTML = `Time: ${m}:${s}`
+
+// }
