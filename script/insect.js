4cf6b47d43ae7c2a04198c9fbee4a033c5889c44
Liana Lanier <favepink12@gmail.com>
2026-03-24 04:42:45 +0000

    Mon, Mar 23, 2026, 9:42 PM -07:00

--- script/insect.js
+++ script/insect.js
@@ -1,51 +1,52 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeE1 = document.getElementById('time')
+const scoreE1 = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
-screens[0].classList.add('up')
-// screens = document.querySelectorAll('.screen')
-// choose_insect_btns = document.querySelectorAll()
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
+})
 
-
-
-
-// score = 0
-// seconds = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const alt = img.getAttribute('alt')
+        const src = img.getAttribute('src')
+        screens[1].classList.add('up')
+        selected_insect = {src, alt}
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
+})
 
 // function startGame() {
 //     setTimeout(createInsect, 1000)
 //     setInterval(increaseTime, 1000)
 // }
 
-// function createInsect() {
-//     const insect = document.createElement('div')
-// }
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const {x, y} = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt" style = "transform: rotate(${Math.random() * 360}deg)" />`
+    game_container.appendChild(insect)
+}
 
-
-
-
-
-
-
-
-
-
-// function getRandomLocation() {
-
-// }
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+    return {x, y}
+}
 
 
 
