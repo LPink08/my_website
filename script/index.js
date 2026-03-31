1324570edd940cca9944c2992209f3c75fd74b57
Liana Lanier <favepink12@gmail.com>
2026-03-31 05:42:34 +0000

    Mon, Mar 30, 2026, 10:42 PM -07:00

--- script/index.js
+++ script/index.js
@@ -4,14 +4,14 @@ hero = document.querySelector('.hero')
 content = document.querySelector('.content')
 body = document.querySelector('body')
 
-changeMode(){
+function changeMode() {
     btn.addEventListener('click', () => {
-    hero.classList.add('active')
-    // nav.classList.add('active2')
-    btn.classList.add('active')
-    content.classList.add('active')
+    hero.classList.add('mode')
+    nav.classList.add('mode')
+    btn.classList.add('mode')
+    content.classList.add('mode')
     body.style.background = '#ecbfff';
-    // nav.classList.add('active3')
+    // nav.classList.add('mode2')
     })
 }
 
@@ -19,11 +19,12 @@ window.addEventListener('scroll', () => {
     console.log(scrollY)
     if (changeMode()){
         if (scrollY > 500){
-            nav.classList.add('active3')
+            nav.classList.add('mode2')
+            nav.classList.remove('mode')
         }
         else {
-            nav.classList.remove('active3')
-            nav.classList.add('active2')
+            nav.classList.remove('mode2')
+            nav.classList.add('mode')
         }
     }
     else {
