9ad2daf13a58864a470760b183ae58bee0ebd982
Liana Lanier <favepink12@gmail.com>
2026-03-31 23:08:41 -0700

    home page

--- script/index.js
+++ script/index.js
@@ -3,76 +3,69 @@ btn = document.querySelector('.btn')
 hero = document.querySelector('.hero')
 content = document.querySelector('.content')
 body = document.querySelector('body')
-// figure out the functions and how to use the if statements to control scrollY
+
 const list = ['pink', 'purple']
-index = 0
+let index = 0
 btn.addEventListener('click', () => {
-    index = (index + 1) % 2
-    if (index == 1){
-        changeMode()
-    }
-    else {
-        fixMode()
-    }
+        index = (index + 1) % 2
+        handleScroll()
+        if (index == 1){
+            changeMode()
+        }
+        else if (index == 0){
+            fixMode()
+        }
 })
 
+handleScroll()
+
+function handleScroll() {
+    if (index == 1){
+        window.removeEventListener('scroll', addScrollYPink)
+        window.addEventListener('scroll', addScrollYPurple)
+    }
+    else if (index == 0){
+        window.removeEventListener('scroll', addScrollYPurple)
+        window.addEventListener('scroll', addScrollYPink)
+    }
+}
+
 function changeMode() {
+    nav.classList.remove('active')
     hero.classList.add('mode')
     nav.classList.add('mode')
     btn.classList.add('mode')
     content.classList.add('mode')
-    body.style.background = '#ecbfff';
+    body.style.background = '#ecbfff'
 }
 
 function fixMode() {
     hero.classList.remove('mode')
     nav.classList.remove('mode')
-    nav.classList.add('active')
     btn.classList.remove('mode')
     content.classList.remove('mode')
-    body.style.background = 'LavenderBlush';
+    nav.classList.remove('mode2')
+    body.style.background = '#ffd4e6'
 }
 
-window.addEventListener('scroll', () => {
+function addScrollYPurple() {
+   console.log(scrollY)
+    if (scrollY > 500){
+        nav.classList.add('mode2')
+        nav.classList.remove('mode')
+    }
+    else {
+        nav.classList.remove('mode2')
+        nav.classList.add('mode')
+    }
+}
+
+function addScrollYPink() {
     console.log(scrollY)
-    if (changeMode()){
-        if (scrollY > 500){
-            nav.classList.add('mode2')
-            nav.classList.remove('mode')
-        }
-        else {
-            nav.classList.remove('mode2')
-            nav.classList.add('mode')
-        }
+    if (scrollY > 500){
+        nav.classList.add('active')
     }
-    else if (fixMode()){
-        if (scrollY > 500) {
-            nav.classList.add('active')
-        }
-        else {
-            nav.classList.remove('active')
-        }
+    else {
+        nav.classList.remove('active')
     }
-})
-
-
-
-// btn.addEventListener('click', () => {
-//     hero.classList.add('mode')
-//     nav.classList.add('mode')
-//     btn.classList.add('mode')
-//     content.classList.add('mode')
-//     body.style.background = '#ecbfff';
-//     // nav.classList.add('mode2')
-//     })
-
-
-// btn.addEventListener('click', () => {
-//     hero.classList.remove('mode')
-//     nav.classList.remove('mode')
-//     nav.classList.add('active')
-//     btn.classList.remove('mode')
-//     content.classList.remove('mode')
-//     body.style.background = 'LavenderBlush';
-//     // nav.classList.remove('mode2')
-//     })
+}
